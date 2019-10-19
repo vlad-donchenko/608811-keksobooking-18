f2f09d1342782c69edbbd0add343c735ea883238
vlad-donchenko <2pi14b.donchenko@gmail.com>
2019-10-19 13:55:55 +0300

    module5-task3 - Реализовал перетаскивание Пина

--- js/map.js
+++ js/map.js
@@ -20,6 +20,43 @@
     }
   };
 
+  mainMarker.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      mainMarker.style.top = (mainMarker.offsetTop - shift.y) + 'px';
+      mainMarker.style.left = (mainMarker.offsetLeft - shift.x) + 'px';
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
   mainMarker.addEventListener('mousedown', onMainMarkerMouseDown);
   mainMarker.addEventListener('keydown', onMainMarkerKeydown);
 
