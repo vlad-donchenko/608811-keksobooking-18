237dfbcd8184632a46905b3b228c9b090402fafa
vlad-donchenko <2pi14b.donchenko@gmail.com>
2019-11-07 10:25:09 +0200

    module7-task2 - изменил названия свойств в priceMap

--- js/filter.js
+++ js/filter.js
@@ -8,11 +8,11 @@
       start: 0,
       end: 1000
     },
-    'midle': {
+    'middle': {
       start: 10000,
       end: 50000
     },
-    'hight': {
+    'high': {
       start: 50000,
       end: Infinity
     }
@@ -20,13 +20,15 @@
 
   var filterRule = {
     'housing-type': function (data, filter) {
+      console.log('housing-type', data.offer.type === filter.value);
       return data.offer.type === filter.value;
     },
     'housing-price': function (data, filter) {
-      console.log(filter.value);
+      console.log('housing-price', data.offer.type === data.offer.price >= priceMap[filter.value].start && data.offer.price < priceMap[filter.value].end);
       return data.offer.price >= priceMap[filter.value].start && data.offer.price < priceMap[filter.value].end;
     },
     'housing-rooms': function (data, filter) {
+      console.log('housing-rooms', parseInt(data.offer.rooms, 10) === filter.value);
       return parseInt(data.offer.rooms, 10) === filter.value;
     },
     'housing-guests': function (data, filter) {
@@ -34,6 +36,7 @@
     },
     'housing-features': function (data, filter) {
       var checkedInputs = Array.from(filter.querySelectorAll('.map__checkbox:checked'));
+      console.log(checkedInputs);
       checkedInputs.every(function (element) {
         return data.offer.features.some(function (features) {
           return features === element.value;
@@ -45,6 +48,8 @@
   var getFilterData = function (data, elements) {
     return data.filter(function (item) {
       return elements.every(function (filter) {
+        console.log(filter);
+        console.log(filter.value);
         console.log((filter.value === 'any') ? true : filterRule[filter.id](item, filter));
         return (filter.value === 'any') ? true : filterRule[filter.id](item, filter);
       });
@@ -53,7 +58,7 @@
 
   var onFormFilterChange = function () {
     var filterElements = Array.from(formFilter.children);
-    console.log(getFilterData(window.map.offers().slice(), filterElements));
+    console.log(getFilterData(window.map.offers(), filterElements));
     //window.map.renderMarkers(filterOffers);
   };
 
